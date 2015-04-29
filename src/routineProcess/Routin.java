2e07913fe2f2728425e874aa76dfeac2875aa1b4
shlomilbz <shllomilbz@gmail.com>
2015-04-29 18:35:47 +0300

    sh 6

--- src/routineProcess/Routin.java
+++ src/routineProcess/Routin.java
@@ -4,14 +4,9 @@ public class Routin {
 
 	//// i know.
 }
-<<<<<<< HEAD
-
 
 public class child {
 	/// you are a child!
 	//// i know.
 }
-=======
-hffhgkjkgk
-flkencln
->>>>>>> branch 'master' of https://github.com/shlomilbz/gisEclipse.git
+
